c1c9fb539a649574920927045dde6b1e870a6347
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 17:22:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,15 +5,3 @@ second_number = 4
 def addition
   sum = first_number + second_number
 end
-
-def subtraction 
-  sum = first_number - second_number 
-end
-
-def multiplication
-  sum = first_number  * second_number 
-end
-
-def division 
-  sum = first_number  / second_number 
-end
