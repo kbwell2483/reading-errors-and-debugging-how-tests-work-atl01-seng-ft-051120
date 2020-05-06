4e7e408d045b60ea64c5e38335f4ba381a37bdf8
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 17:12:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,18 @@
 first_number = 3 
 second_number = 4
+
 def
   sum = first_number + second_number
 end
 
 def
   sum = first_number - second_number
-sum = first_number * second_number
-sum = first_number / second_number
+end
+
+def
+  sum = first_number * second_number
+end
+
+def
+  sum = first_number / second_number
+end
