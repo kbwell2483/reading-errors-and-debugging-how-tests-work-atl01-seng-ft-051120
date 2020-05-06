3e9a5b35b7474b4a32a9107b01a0e38063e86ac1
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 17:17:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,14 +6,14 @@ def addition(first_number, second_number)
   first_number  + second_number 
 end
 
-def
-  sum = first_number - second_number
+def subtraction(first_number, second_number) 
+  first_number - second_number 
 end
 
-def
-  sum = first_number * second_number
+def multiplication(first_number, second_number) 
+  first_number  / second_number 
 end
 
-def
-  sum = first_number / second_number
+def division(first_number, second_number) 
+  first_number  / second_number 
 end
