dc2f0f2c6fcf9f9db9ee7369f49fb913116b6148
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 17:19:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,8 @@ first_number = 3
 second_number = 4
 
 def addition(first_number, second_number) 
-  sum = 
+  sum = first_number + second_number
+  puts sum
 end
 
 def subtraction(first_number, second_number) 
